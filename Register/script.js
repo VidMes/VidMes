7bbaf2c274f1d8d6c368eaae4dd791149de73529
VidMes <119504497+VidMes@users.noreply.github.com>
2023-06-03 10:35:51 +0700

    Update script.js

--- Register/script.js
+++ Register/script.js
@@ -1,22 +1,16 @@
 const backgroundVideo = document.getElementById('background-video');
 
-// Mendapatkan waktu saat ini
 const currentTime = new Date().getHours();
 
-// Mendefinisikan video yang sesuai dengan waktu saat ini
 let videoSource = '';
 if (currentTime >= 5 && currentTime < 12) {
-    // Pagi
     videoSource = 'morning.mp4';
 } else if (currentTime >= 12 && currentTime < 18) {
-    // Sore
     videoSource = 'afternoon.mp4';
 } else {
-    // Malam
     videoSource = 'night.mp4';
 }
 
-// Mengubah sumber video
 backgroundVideo.src = videoSource;
 
 var page1 = document.getElementById("page1");
